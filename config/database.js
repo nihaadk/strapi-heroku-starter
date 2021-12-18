2d12c7285c38c4b8a85859364396d2df915900c9
nihaadk <nihadkeric@gmail.com>
2021-12-18 22:20:05 +0100

    created post schema

--- config/database.js
+++ config/database.js
@@ -1,20 +1,23 @@
 const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
+// const config = parse(process.env.DATABASE_URL);
+const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
+    // client: "postgres",
+    client: 'sqlite',
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      // host: config.host || '127.0.0.1',
+      // port: config.port || 27017,
+      // database: config.database,
+      // user: config.user,
+      // password: config.password,
+      // ssl: {
+      //   rejectUnauthorized: false,
+      // },
     },
-    debug: false,
+    // debug: false,
     useNullAsDefault: true,
   },
 });
