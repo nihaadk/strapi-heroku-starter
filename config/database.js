9525add96121dd73aa33c5d25b17840877d17dcb
nihaadk <nihadkeric@gmail.com>
2021-12-18 22:21:46 +0100

    config: database change

--- config/database.js
+++ config/database.js
@@ -1,23 +1,23 @@
 const parse = require("pg-connection-string").parse;
-// const config = parse(process.env.DATABASE_URL);
-const path = require('path');
+const config = parse(process.env.DATABASE_URL);
+// const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    // client: "postgres",
-    client: 'sqlite',
+    client: "postgres",
+    // client: 'sqlite',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-      // host: config.host || '127.0.0.1',
-      // port: config.port || 27017,
-      // database: config.database,
-      // user: config.user,
-      // password: config.password,
-      // ssl: {
-      //   rejectUnauthorized: false,
-      // },
+      // filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
-    // debug: false,
+    debug: false,
     useNullAsDefault: true,
   },
 });
