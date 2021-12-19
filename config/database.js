3dae8f5d311a828b49db142622809a98c7554ee3
nihaadk <nihadkeric@gmail.com>
2021-12-19 13:03:17 +0100

    config: change database path for local develop

--- config/database.js
+++ config/database.js
@@ -1,23 +1,16 @@
 const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
-// const path = require('path');
+const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
-    // client: 'sqlite',
+    client: "sqlite",
     connection: {
-      // filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
     },
-    debug: false,
     useNullAsDefault: true,
   },
 });
