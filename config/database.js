453cb4b606c3d51cf5767981168514481313b30f
nihaadk <nihadkeric@gmail.com>
2021-12-19 13:29:19 +0100

    config: update database.js and added header site

--- config/database.js
+++ config/database.js
@@ -1,15 +1,40 @@
-const path = require('path');
+const path = require("path");
+const parse = require("pg-connection-string").parse;
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: "sqlite",
-    connection: {
-      filename: path.join(
-        __dirname,
-        "..",
-        env("DATABASE_FILENAME", ".tmp/data.db")
-      ),
-    },
-    useNullAsDefault: true,
-  },
-});
+
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "development") {
+    return {
+      connection: {
+        client: "sqlite",
+        connection: {
+          filename: path.join(
+            __dirname,
+            "..",
+            env("DATABASE_FILENAME", ".tmp/data.db")
+          ),
+        },
+        useNullAsDefault: true,
+      },
+    };
+  } else {
+    const config = parse(process.env.DATABASE_URL);
+    return {
+      connection: {
+        client: "postgres",
+        connection: {
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          user: config.user,
+          password: config.password,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+        debug: false,
+        useNullAsDefault: true,
+      },
+    };
+  }
+};
